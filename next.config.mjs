3761078499ac45bf046dd20973a88681157ac00f
shaodongmoney-code <shaodongmoney@gmail.com>
2026-06-13 19:50:17 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,7 +9,3 @@ const nextConfig = {
 }
 
 export default nextConfig
-
-// Enable Cloudflare bindings in `next dev` when developing for OpenNext.
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
-initOpenNextCloudflareForDev()
